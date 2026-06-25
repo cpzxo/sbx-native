e5cab1f896c07b07ffd663eb403e5eae01be1c20
cpzxo <acglove000@gmail.com>
2026-06-25 17:14:21 +0800

    Update App.java

--- java/src/main/java/dev/sbxnative/App.java
+++ java/src/main/java/dev/sbxnative/App.java
@@ -44,14 +44,14 @@ public class App {
     private static final boolean AUTO_ACCESS = envBool("AUTO_ACCESS", false);
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
-    private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "0a6568ff-ea3c-4271-9020-450560e10d61");
+    private static final String SUB_PATH = env("SUB_PATH", "cpz");
+    private static final String UUID = env("UUID", "d26d2ee6-8757-4ee8-9918-0da6d818f0d7");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
-    private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "rustix.cpz.ccwu.cc");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZTI5ZTEwYzI1ZjgzMThmYWFmNmJiNzRiZDdiOTE2MjMiLCJ0IjoiMjMxZjcyNTgtOGVlYi00ZjU5LWE0MjQtYWVlZjM3NTcwYmExIiwicyI6IllXVmhNRGd3TXpZdE1tSTFZUzAwTm1SbUxXSXlNMkV0T0ROak1UVTNOREF4TkdVMiJ9");
+    private static final int ARGO_PORT = envInt("ARGO_PORT", 8010);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
@@ -60,9 +60,9 @@ public class App {
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
     private static final int PORT = envInt("PORT", 3000);
-    private static final String NAME = env("NAME", "");
-    private static final String CHAT_ID = env("CHAT_ID", "");
-    private static final String BOT_TOKEN = env("BOT_TOKEN", "");
+    private static final String NAME = env("NAME", "Rustix");
+    private static final String CHAT_ID = env("CHAT_ID", "7573878464");
+    private static final String BOT_TOKEN = env("BOT_TOKEN", "8366489012:AAESpCBZxSavhRTe08D_T_zwhe6BuwzDMR4");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
 
     private static final Path ROOT = Path.of("").toAbsolutePath();
